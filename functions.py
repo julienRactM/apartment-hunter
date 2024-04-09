2a2c1cfbc09cd2aa9401c09ac61476465501cc6b
Julien RACT MUGNEROT <julien.ract.mugnerot@MACC65V9XY3DG.local>
2024-04-09 18:22:55 +0200

    [Update] Select Kbset Variance Treshold

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 import joblib
 import os
 
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_regression, f_classif, chi2
+
+
+
 def save_model_if_best(new_score, pipeline, model_name):
     # joblib model only, could add a param joblib/pickle options
     files = os.listdir('app/app_model')
@@ -37,3 +41,53 @@ def load_best_model(model_name=False, path='app/app_model/'):
                 score_dict[f.split('_')[1]] = int(f.split('_')[0])
 
         return joblib.load(f"{path}{score_dict[min(score_dict, key=score_dict.get)]}_{min(score_dict, key=score_dict.get)}_model.pkl")
+
+
+# price_column is a str name of the numeric column
+def iqr_range_price_filter(df, price_column):
+    q3 = df.price.describe()['75%']
+    q1 = df.price.describe()['25%']
+    iqr_range = q3-q1
+    new_df = df[(df[price_column] > q1-iqr_range*1.5) & (df[price_column] < q3+(iqr_range*1.5))]
+    print("old number of rows", df.shape[0])
+    print("new number of rows", new_df.shape[0])
+    return new_df
+
+
+
+def variance_threshold_selector(data, threshold=0.2):
+    selector = VarianceThreshold(threshold)
+    selector.fit(data)
+    new_df = data[data.columns[selector.get_support(indices=True)]]
+    dropped_features = [feature for feature in data.columns if feature not in new_df.columns]
+    if dropped_features:
+        print('columns dropped :', dropped_features)
+    else :
+        print('no features dropped')
+    return new_df
+
+# k is number of feature to keep
+def KBest_selector(data, target_name, k=1, score_function = 'f_regression'):
+    # Create and fit selector
+    if score_function == 'f_regression':
+        selector = SelectKBest(f_regression, k=k)
+    elif score_function == 'f_classif':
+        selector = SelectKBest(f_classif, k=k)
+    else :        
+        selector = SelectKBest(chi2, k=k)
+        print('using chi2')
+        
+    selector.fit(data.drop(columns=[target_name]), data[target_name])
+    # Get columns to keep and create new dataframe with those only
+    cols_idxs = selector.get_support(indices=True)
+    new_df = data.iloc[:,cols_idxs]
+    
+    # display the names of dropped columns
+    dropped_features = [feature for feature in data.columns if feature not in new_df.columns].remove(target_name)
+    if dropped_features:
+        print('columns dropped :', dropped_features)
+    else :
+        print('no features dropped')
+        
+    new_df.loc[:,target_name] = data.loc[:, target_name]
+    return new_df
